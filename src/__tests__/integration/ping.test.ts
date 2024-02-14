c86b893b461f13b816e8a6848aa5e23d7a5ce17b
Sunday Ezeilo <ezeilo.su@gmail.com>
2024-02-14 15:18:39 +0000

    Add test to /ping

---
+++ src/__tests__/integration/ping.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import httpStatus from 'http-status';
+
+import { app } from '../../app';
+
+describe('Test Server Endpoints', () => {
+  test('GET /ping endpoint returns status code 200', async () => {
+    const response = await request(app).get('/api/v1/ping');
+    expect(response.status).toBe(httpStatus.OK);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        data: {
+          version: expect.stringMatching(/[a-zA-Z0-9]+/)
+        }
+      })
+    );
+  });
+});
