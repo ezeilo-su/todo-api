6f822e5a5be1c6151517e8ce38ad5e52d8cee96d
Sunday Ezeilo <ezeilo.su@gmail.com>
2024-02-15 13:46:47 +0000

    update ping route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Express } from 'express';
 
 import { pingRoute } from './ping.route';
 
-const API_PREFIX = '/api/v1';
+const API_PREFIX = '/api';
 
 const bindRoutes = (app: Express) => {
   app.use(`${API_PREFIX}/ping`, pingRoute);
