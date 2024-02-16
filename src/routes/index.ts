eca907f0a121c44fb6fa2191574d9878af1ccb15
Sunday Ezeilo <ezeilo.su@gmail.com>
2024-02-16 13:10:16 +0000

    Handle unsupported routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Express } from 'express';
+import httpStatus from 'http-status';
 
 import { pingRoute } from './ping.route';
 
@@ -6,6 +7,10 @@ const API_PREFIX = '/api';
 
 const bindRoutes = (app: Express) => {
   app.use(`${API_PREFIX}/ping`, pingRoute);
+
+  app.get('*', (_reqq, res) => {
+    res.status(httpStatus.NOT_FOUND).send('Not found');
+  });
 };
 
 export { bindRoutes };
