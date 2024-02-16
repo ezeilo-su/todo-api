259d79ef35d49a3119847fa3caa1412bc78bd9f0
Sunday Ezeilo <ezeilo.su@gmail.com>
2024-02-16 13:58:04 +0000

    Debug reading commit hash

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,8 +4,8 @@ import { AppEnv } from '../types/enums';
 
 export const getCommitHash = (): string => {
   if (process.env.APP_ENV !== AppEnv.staging && process.env.APP_ENV !== AppEnv.production) {
+    return execSync('git rev-parse --short HEAD').toString().trim();
   }
-  return execSync('git rev-parse --short HEAD').toString().trim();
 
-  // return process.env.COMMIT_SHA as string;
+  return process.env.HEROKU_SLUG_COMMIT as string;
 };
